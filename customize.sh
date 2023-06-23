d2f468c0ecb3f32969e5c6333584b07d18c51915
Drixn <admin@drixn.com>
2023-06-23 16:29:41 +0800

    修改默认IP

--- customize.sh
+++ customize.sh
@@ -6,7 +6,7 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
 
 #2 Set DISTRIB_REVISION
 sed -i "s/OpenWrt /Deng Build $(TZ=UTC-8 date "+%Y.%m.%d") Compiled by Drixn /g" package/lean/default-settings/files/zzz-default-settings
