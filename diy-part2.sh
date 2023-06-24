5ee2f88f90487534dfcd9c2d15efcce8ea943640
Drixn <i@drixn.com>
2023-06-24 16:43:45 +0800

    优化脚本

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,9 +17,10 @@ sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-defaul
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 
 # Set DISTRIB_REVISION
-sed -i "s/OpenWrt /Deng Build $(TZ=UTC-8 date "+%Y.%m.%d") Compiled by Drixn /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt_x86_64 /Build $(TZ=UTC-8 date "+%Y.%m.%d") Compiled by Drixn /g" package/lean/default-settings/files/zzz-default-settings
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 10.10.10.2）
+echo '自定义 LAN 口 IP...'
 sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
 
 # Modify system hostname（FROM OpenWrt CHANGE TO OpenWrt-x86_64）
